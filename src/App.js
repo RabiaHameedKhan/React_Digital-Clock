a41918fee5fa75e937196c1d58dd3b59f6004bf6
RabiaHameedKhan <rabiahameedkhan27@gmail.com>
2024-09-12 09:10:22 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//...........Hooks Basic...............
+
+// const App = ()=>{
+
+//     // const [state]= useState;
+//     const [count, setCount]= useState(0);
+
+//     const IncNum=()=> {
+//         setCount(count + 1);
+//     };
+   
+    
+
+
+
+//     return(
+
+//         <>
+//         <h1>You have clicked {count} times.</h1>
+//         <div className="container">
+//         <button onClick={IncNum}>Click Here!</button>
+//         </div>
+
+//         </>
+//     );
+// };
+
+
+//............Digital Clock....................
+
+const App= () => {
+
+    let time= new Date().toLocaleTimeString();
+
+    const [currtime, setcurrtime]= useState(time);
+
+    const Updatetime= ()=>{
+        time= new Date().toLocaleTimeString();
+        setcurrtime(time);
+    }
+   
+    setInterval(Updatetime, 1000);
+
+    return(
+        <>
+        <h1>
+            DIGITAL CLOCK
+        </h1>
+        <div className="container">
+        <h1>{currtime}</h1>
+        </div>
+        </>
+    )
+
 }
 
 export default App;
